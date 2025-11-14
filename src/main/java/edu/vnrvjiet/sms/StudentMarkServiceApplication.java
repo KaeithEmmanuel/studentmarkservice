c61660d72fa8fa6a2b0c280866a2732159443fea
KaeithEmmanuelEddu <kaeithemmanueleddu@gmail.com>
2025-11-15 00:10:18 +0530

    commit1

--- src/main/java/edu/vnrvjiet/sms/StudentMarkServiceApplication.java
+++ src/main/java/edu/vnrvjiet/sms/StudentMarkServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentMarkServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StudentMarkServiceApplication.class, args);
         System.out.println("Tomcat running for Student Mark Microservice... Testing");
